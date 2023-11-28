595ea80b29785a1989e782962510950e08c08f14
Lou DeGenaro <lou.degenaro@gmail.com>
2023-11-28 10:52:54 -0500

    fix: release
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/release.sh
+++ scripts/automation/release.sh
@@ -2,18 +2,24 @@
 
 source config.env
 
-version_tag=$(semantic-release print-version)
-echo "Bumping version of profiles to ${version_tag}" 
-export VERSION_TAG="$version_tag"
-echo "VERSION_TAG=${VERSION_TAG}" >> $GITHUB_ENV
-
-COUNT=$(ls -l md_profiles | grep ^- | wc -l)
-if [ $COUNT -lt 1 ]
+COUNT_PROFILES=$(ls -l catalogs | grep ^- | wc -l)
+COUNT_PROFILE_MD=$(ls -l md_catalogs | grep ^- | wc -l)
+if [ "$COUNT_PROFILES" == "0" ] || [ "$COUNT_PROFILE_MD" == "0" ]
 then
-	./scripts/automation/regenerate_profiles.sh $version_tag
+    echo "no profile or markdown present -> nothing to do"
+else
+    version_tag=$(semantic-release print-version)
+	echo "Bumping version of profiles to ${version_tag}" 
+	export VERSION_TAG="$version_tag"
+	echo "VERSION_TAG=${VERSION_TAG}" >> $GITHUB_ENV
+	# There is no md but json has at least one control
+	COUNT=$(ls -l md_profiles | grep ^- | wc -l)
+	if [ $COUNT -lt 1 ]
+	then
+		./scripts/automation/regenerate_profiles.sh 
+	fi
+	./scripts/automation/assemble_profiles.sh $version_tag
+	git config --global user.email "$EMAIL"
+	git config --global user.name "$NAME" 
+	semantic-release publish
 fi
-
-./scripts/automation/assemble_profiles.sh $version_tag
-git config --global user.email "$EMAIL"
-git config --global user.name "$NAME" 
-semantic-release publish
