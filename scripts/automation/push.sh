39810ba3de98b15817c34f303966fd5582dc8daf
Lou DeGenaro <lou.degenaro@gmail.com>
2023-11-28 15:30:32 -0500

    fix: add check for nothing to do in push
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/push.sh
+++ scripts/automation/push.sh
@@ -86,4 +86,12 @@ function err() {
     msg "$*" 1>&2
 }
 
-github-branch-commit
+COUNT_PROFILES=$(ls -l profiles | grep ^- | wc -l)
+COUNT_PROFILE_MD=$(ls -l md_profiles | grep ^- | wc -l)
+if [ "$COUNT_PROFILES" == "0" ] || [ "$COUNT_PROFILE_MD" == "0" ]
+then
+    echo "no profile or markdown present -> nothing to do"
+else
+	github-branch-commit
+fi
+
